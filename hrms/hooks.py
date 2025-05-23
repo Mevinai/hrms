7a52e59ebc26f64b2d4565c2a2c73b353822354b
Mesfin Tsegaye <36897190+codewithmesfin@users.noreply.github.com>
2025-05-23 18:15:54 +0300

    Update hooks.py

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -1,5 +1,5 @@
 app_name = "hrms"
-app_title = "Frappe HR"
+app_title = "OIPDC HR"
 app_publisher = "Frappe Technologies Pvt. Ltd."
 app_description = "Modern HR and Payroll Software"
 app_email = "contact@frappe.io"
@@ -12,8 +12,8 @@ app_home = "/app/overview"
 add_to_apps_screen = [
 	{
 		"name": "hrms",
-		"logo": "/assets/hrms/images/frappe-hr-logo.svg",
-		"title": "Frappe HR",
+		"logo": "/files/oipdc.png",
+		"title": app_title,
 		"route": "/app/overview",
 		"has_permission": "hrms.hr.utils.check_app_permission",
 	}
